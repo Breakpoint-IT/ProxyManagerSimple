fecd52f29191c770266b1d07e0d61e59f16404d0
Marek Tonino - SELCO <monio1997@gmail.com>
2023-07-19 17:25:24 +0200

    .

---
+++ src/proxyManagerMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace test
+{
+    partial class proxyManagerMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(proxyManagerMain));
+            this.lblProxyStatus2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblProxyStatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblProxyStatus2
+            // 
+            this.lblProxyStatus2.AutoSize = true;
+            this.lblProxyStatus2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProxyStatus2.Location = new System.Drawing.Point(6, 25);
+            this.lblProxyStatus2.Name = "lblProxyStatus2";
+            this.lblProxyStatus2.Size = new System.Drawing.Size(109, 18);
+            this.lblProxyStatus2.TabIndex = 0;
+            this.lblProxyStatus2.Text = "Proxy Status:";
+            this.lblProxyStatus2.Click += new System.EventHandler(this.lblProxyStatus_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Proxy deaktivieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(301, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Proxy aktivieren";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblProxyStatus
+            // 
+            this.lblProxyStatus.AutoSize = true;
+            this.lblProxyStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProxyStatus.Location = new System.Drawing.Point(121, 25);
+            this.lblProxyStatus.Name = "lblProxyStatus";
+            this.lblProxyStatus.Size = new System.Drawing.Size(54, 18);
+            this.lblProxyStatus.TabIndex = 3;
+            this.lblProxyStatus.Text = "status";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblProxyStatus2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.lblProxyStatus);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 115);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Proxy Manager";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current Version: 2.1.0.0";
+            // 
+            // proxyManagerMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 156);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "proxyManagerMain";
+            this.Text = "Proxy Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProxyStatus2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblProxyStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ src/proxyManagerMain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+namespace test
+{
+    public partial class proxyManagerMain : Form
+    {
+        public proxyManagerMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Prosy Status - label
+
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            int proxyStatus = (int)registry.GetValue("ProxyEnable");
+            if (proxyStatus == 0)
+                lblProxyStatus.Text = "inaktiv ";
+            if (proxyStatus == 1)
+                lblProxyStatus.Text = " aktiv";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //////////////////////
+            /// disable Proxy ///
+            /////////////////////
+            
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);   
+            registry.SetValue("ProxyServer", "");
+            registry.SetValue("ProxyEnable", 0);
+            registry.SetValue("ProxyOverride", "");
+
+            //Proxy Status - label
+
+            int proxyStatus = (int)registry.GetValue("ProxyEnable");
+            if (proxyStatus == 0)
+                lblProxyStatus.Text = "inaktiv ";
+            if (proxyStatus == 1)
+                lblProxyStatus.Text = " aktiv";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //////////////////////
+            /// activate Proxy ///
+            /////////////////////
+            
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);         
+            registry.SetValue("ProxyServer", "10.244.8.240:3128");
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyOverride", "10.*;*.kvnbw.de;*.iteos.net;dcserver*;*.kaisersbach.int;*komm.one;0.de.pool.ntp.org;1.de.pool.ntp.org;2.de.pool.ntp.org;3.de.pool.ntp.org;*.local.pcvisit.com;*.komm-one.net");
+
+            //Proxy Status - label
+
+            int proxyStatus = (int)registry.GetValue("ProxyEnable");
+            if (proxyStatus == 0)
+                lblProxyStatus.Text = "inaktiv ";
+            if (proxyStatus == 1)
+                lblProxyStatus.Text = " aktiv";
+        }
+
+        private void lblProxyStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
